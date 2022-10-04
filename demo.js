bcc7d02ce6626a5f891548ef87e129ac6d0799a3
Ankitadounde <ankitadounde88@gmail.com>
2022-10-04 17:45:59 +0530

    basic js
    basic js

---
+++ demo.js
@@ -0,0 +1,11 @@
+
+console.log("My full name - Ankita Ravindra Dounde");
+console.log("My hobbies 1.Cycling 2.Cooking 3.Road trip");
+console.info("My personal strengths 1.Creativity 2.Patience 3.Dedication");  
+console.warn("My weaknesses 1.Shorttemper 2.Over thinker 3.Sensetive");
+console.log(30+50);
+console.log(49*479);
+
+
+
+
